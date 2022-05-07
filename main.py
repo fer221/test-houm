80c922dec44868118512c9310e06d6507fc244d9
fer221 <fernanda.1991.alejandra@gmail.com>
2022-05-07 13:32:30 -0700

    respuesta 1 completa

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import requests
 import json
+from collections import defaultdict
+
+#Obtén cuantos pokemones poseen en sus nombres “at” y tienen 2 “a” en su nombre, incluyendo la primera del “at”. Tu respuesta debe ser un número.
 
 if __name__ == '__main__':
     url = 'https://pokeapi.co/api/v2/pokemon-form'
@@ -10,10 +13,16 @@ if __name__ == '__main__':
         response_json = response.json()
         result = response_json.get('results',[])
 
+        list_name = []
         if result:
             for pokemon_form in result:
                 name_pokemon = pokemon_form['name']
-                list_name = []
                 list_name.append(name_pokemon)
-                print(list_name)
+
+        list_pokemon_at = []
+        count = 0
+        for letras in list_name:
+            if "at" in letras and letras.count('a') == 2:
+                count += 1
+        print("la cantidad de pokemones que contienen AT y tienen 2 A:", count)
                 
