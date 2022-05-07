e7febb459583d7ba9f929fa81555916fe5056961
fer221 <fernanda.1991.alejandra@gmail.com>
2022-05-07 13:23:40 -0700

    respuesta 3 completa

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from collections import defaultdict
 #Obtén cuantos pokemones poseen en sus nombres “at” y tienen 2 “a” en su nombre, incluyendo la primera del “at”. Tu respuesta debe ser un número.
 
 if __name__ == '__main__':
-    url = 'https://pokeapi.co/api/v2/pokemon/ditto'
+    url = 'https://pokeapi.co/api/v2/pokemon-form'
     
     response = requests.get(url)
     if response.status_code == 200:
@@ -24,7 +24,6 @@ if __name__ == '__main__':
         for letras in list_name:
             if "at" in letras and letras.count('a') == 2:
                 count += 1
-                print(letras)
         print("la cantidad de pokemones que contienen AT y tienen 2 A:", count)
 
 
